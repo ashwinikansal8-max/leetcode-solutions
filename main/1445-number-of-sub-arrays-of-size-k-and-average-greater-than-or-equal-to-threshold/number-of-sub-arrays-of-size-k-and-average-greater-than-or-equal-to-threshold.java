cc157af8c01f9d722d6be8e436f1301620e2ffc7
Ashwini Kansal <ashwinikansal8@gmail.com>
2026-09-07 03:50:33 +0530

    Time: 3 ms (87.37%) | Memory: 71.6 MB (90.84%) - LeetSync

--- main/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ main/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -9,7 +9,7 @@ class Solution {
 
         for(int i=0;i<=n-k;i++)
         {
-          if((double)s/k >= threshold) m++;
+          if(s>= threshold*k) m++;
             
             s = s- nums[i] + nums[(i+k)%n];
         }
