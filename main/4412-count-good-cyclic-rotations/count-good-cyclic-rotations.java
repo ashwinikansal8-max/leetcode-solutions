d485b8205d514b838e1fb3fae7c09c1c8d2f22a8
Ashwini Kansal <ashwinikansal8@gmail.com>
2026-09-06 18:34:16 +0530

    Time: 3 ms (96.77%) | Memory: 71.5 MB (68.77%) - LeetSync

--- main/4412-count-good-cyclic-rotations/count-good-cyclic-rotations.java
+++ main/4412-count-good-cyclic-rotations/count-good-cyclic-rotations.java
@@ -1,7 +1,8 @@
 class Solution {
     public int countGoodRotations(int[] nums) {
         int n= nums.length,k=n/2;
-        int fh=0,sh=0,ts=0,c=0;
+        long fh=0,sh=0,ts=0;
+        int c=0;
         for(int i=0;i<k;i++) fh+=nums[i];
 
         for(int i=0;i<n;i++) ts+=nums[i];
